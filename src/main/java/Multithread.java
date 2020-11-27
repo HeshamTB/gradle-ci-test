a243bd8a904c3c1b206d86faf79c476bdd42a64f
HeshamTB <hishaminv@gmail.com>
2020-11-28 02:10:04 +0300

    init2

--- src/main/java/Multithread.java
+++ src/main/java/Multithread.java
@@ -1,2 +1,29 @@
+import java.util.ArrayList;
+
 public class Multithread {
+
+    public static void main(String[] args) {
+        ArrayList<Thread> pool = new ArrayList<>();
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                for (long i = 0; i < 900000; i++) {
+                    System.out.printf("Thread[%s] %d\n",Thread.currentThread().getName(),i);
+                }
+            }
+        };
+        for (int i = 0; i < 4; i++) {
+            Thread t = new Thread(r);
+            t.setName(String.valueOf(i));
+            pool.add(t);
+            t.start();
+        }
+        for (Thread t : pool){
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
